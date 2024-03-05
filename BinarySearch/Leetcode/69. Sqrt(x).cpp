a74ce31e62599d5817832cffe7b7692491d84de6
FaisalRajpoot1 <144609495+FaisalRajpoot1@users.noreply.github.com>
2024-03-05 21:21:33 +0500

    Create 69. Sqrt(x).cpp

---
+++ BinarySearch/Leetcode/69. Sqrt(x).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+        
+        int start = 1;
+        int end = x;
+        int ans = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (mid <= x / mid) {
+                start = mid + 1;
+                ans = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+
