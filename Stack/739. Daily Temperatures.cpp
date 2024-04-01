83aa42efe9c64692432ab5f7e8dfbbf79fa51d79
FaisalRajpoot1 <144609495+FaisalRajpoot1@users.noreply.github.com>
2024-04-01 21:37:25 +0500

    Create 739. Daily Temperatures.cpp

---
+++ Stack/739. Daily Temperatures.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        stack<pair<int , int>> stk;
+        
+        int n = temp.size();
+        vector<int> res(n);
+        
+        for (int i = 0; i < n; i++) {
+            
+            int curDay = i; 
+            int curTemp = temp[i];
+            
+            while (!stk.empty() && curTemp > stk.top().second) {
+                int prevTemp = stk.top().second;
+                int prevDay = stk.top().first;
+                
+                stk.pop();
+                
+                res[prevDay]  = curDay - prevDay;
+            }
+            
+            stk.push({curDay, curTemp});
+        }
+        return res;
+    }
+    
+    
+};
